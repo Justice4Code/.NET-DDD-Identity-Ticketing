6147920735cb8ea478839975b0b533fade4110ca
Masoud Andalibi <MasoudAndalibii@gmail.com>
2020-07-08 09:37:27 +0430

    added title to ticket entity

--- src/Mega.Ticketing.Domain/Entities/Ticket.cs
+++ src/Mega.Ticketing.Domain/Entities/Ticket.cs
@@ -13,6 +13,7 @@ namespace Mega.Ticketing.Domain.Entities
         public string UserId { get; set; }
         public string Code { get; set; }
         public string UserName { get; set; }
+        public string Title { get; set; }
 
         //Foreign Keys
         public Guid CartableId { get; set; }
